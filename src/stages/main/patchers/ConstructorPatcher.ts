b43dbde30c0318aa4d27672a8fc3609ce537c875
Dan Washusen <dan@reactive.org>
2022-04-27 08:30:55 +1000

    fix: ensure that bound methods are placed after the call to super to comply with ES6 requirements
    Addresses the case noted here https://github.com/decaffeinate/decaffeinate/issues/2141#issuecomment-1109207908.
    Note, this assumes we're always sourcing from CS2 (e.g. --use-cs2).

--- src/stages/main/patchers/ConstructorPatcher.ts
+++ src/stages/main/patchers/ConstructorPatcher.ts
@@ -29,7 +29,8 @@ export default class ConstructorPatcher extends ObjectBodyMemberPatcher {
 
     if (this.expression.body) {
       const linesToInsert = this.getLinesToInsert();
-      this.expression.body.insertStatementsAtIndex(linesToInsert, 0);
+      const insertIndex = this.getIndexOfSuperStatement() + 1;
+      this.expression.body.insertStatementsAtIndex(linesToInsert, insertIndex);
       super.patch(options);
     } else {
       super.patch(options);
